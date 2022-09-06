1eec3868d6eee50b896c03443dd0b845f2f50922
sebastian medina <sebastianlopez_21@hotmail.com>
2022-09-06 10:06:40 -0400

    commit tree dd3ee1d6b0e313dea72a2c9a242e367b11dde7ad

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => renderBooks(data));
   
+    
 }
 
 function renderBooks(books) {
+  //fetchBooks();
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -15,3 +20,7 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+//const booksData = {fetchBooks()};
+
+//renderBooks(books);
